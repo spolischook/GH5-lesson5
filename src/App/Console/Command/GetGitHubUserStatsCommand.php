3426aa18530310cb6c6513cc55911d15f3830862
spolischook <spolischook@gmail.com>
2015-10-31 10:38:01 +0200

    Update console command for get stats

--- src/App/Console/Command/GetGitHubUserStatsCommand.php
+++ src/App/Console/Command/GetGitHubUserStatsCommand.php
@@ -7,20 +7,75 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Yaml\Yaml;
 
 class GetGitHubUserStatsCommand extends Command
 {
-    protected function configure()
+    /** @var array  */
+    private $parameters;
+
+    /** @var  \MongoDB */
+    private $db;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function __construct($name = null)
     {
-        $this
-            ->setName('get-github-user-stats')
-            ->setDescription('Get user stats from GitHub.com and store it to data storage')
-        ;
+        $this->initDb();
+
+        parent::__construct('get-github-user-stats');
     }
 
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @throws
+     * @return void
+     */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $output->writeln('get GitHub stats');
+
+        $githubClient = new \Github\Client();
+
+        foreach ($this->getParameters()["students_github_usernames"] as $username) {
+            /** @var \Github\Api\User $ghUser */
+            $ghUser = $githubClient->api('users');
+            $repositories = $ghUser->repositories($username, 'all');
+
+            var_dump(count($repositories));exit;
+        }
+    }
+
+    /**
+     * @return array
+     * @throws
+     */
+    protected function getParameters()
+    {
+        if (!$this->parameters) {
+            if (!$parametersYmlFile = realpath(__DIR__."/../../../../parameters.yml")) {
+                throw \InvalidArgumentException('Can\'t find parameters.yml file');
+            }
+
+            $this->parameters = Yaml::parse($parametersYmlFile);
+        }
+
+        return $this->parameters;
+    }
+
+    /**
+     * @return \MongoDB
+     */
+    protected function initDb()
+    {
+        $m = new \MongoClient();
+        $dbName = $this->getParameters()['database']['name'];
+
+        $this->db = $m->$dbName;
+
+        return $this;
     }
 }
 
